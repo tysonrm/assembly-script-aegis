82fcdb683db6711480143a5426fc5d6c07525b45
tyson <tyson7757@comcast.net>
2021-07-31 06:00:09 -0500

    test

--- dist/main.js
+++ dist/main.js
@@ -573,7 +573,7 @@ else {}
   \******************/
 /*! unknown exports (runtime-defined) */
 /*! runtime requirements: module, __webpack_require__ */
-/*! CommonJS bailout: module.exports is used directly at 9:0-14 */
+/*! CommonJS bailout: module.exports is used directly at 7:0-14 */
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 var __dirname = "/";
@@ -581,9 +581,7 @@ var fs = __webpack_require__(/*! fs */ "fs");
 
 var loader = __webpack_require__(/*! @assemblyscript/loader */ "./node_modules/@assemblyscript/loader/umd/index.js");
 
-var imports = {
-  add: add
-};
+var imports = {};
 var wasmModule = loader.instantiateSync(fs.readFileSync(__dirname + "/build/optimized.wasm"), imports);
 module.exports = wasmModule.exports;
 
