d2a1f6b9245421526b90a587c29119d159b67f44
tyson <tyson7757@comcast.net>
2021-07-31 19:28:23 -0500

    wasm

--- assembly/index.ts
+++ assembly/index.ts
@@ -3,3 +3,7 @@
 export function add(a: i32, b: i32): i32 {
   return a + b;
 }
+
+// export function makeFactory(dependencies: Array<Object>) {
+//   return Model factory(request: __stack_pointer);
+// }`
